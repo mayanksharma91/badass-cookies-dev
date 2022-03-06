f1e0160895df755ccbb0738584f4f4883d6dd196
Mayank Sharma <mayank.sharma.bits@gmail.com>
2022-03-06 19:41:45 +0530

    create a command called test

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
 # badass-cookies
 
 ## Dependencies
-- npm (used8.5.2)
+- npm (used 8.5.2)
 - nodejs (used v16.14.0)
 - supabase
+- telegraf
 
 
 ## Installations/terminal commands
-`npm init -y`
-This created the package.json file with some config
+- `npm init -y`
+    - This created the package.json file with some config
+
+- `npm dotenv`
+    - Reads in .env file
+- Created added node file names to .gitignore using VS code plugin
+
+- `npm isntall telegraf --save`
+    - Installs a package library for Telegram and adds it to the package.json dependencies
--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 // Require dotenv package that will help read API keys from the .env file
 // The browser does not support process.env, hence we need dotenv
-require('dotenv').config()
+require('dotenv').config();
 // console.log(process.env)
 
 // Require Telegraf package by creating a telegraf object
 // Constructor functions generally start with an upper case letter
 // TODO understand why we need the curly braces in const {Telegraph}
-const {Telegraf} = require('telegraf')
+const {Telegraf} = require('telegraf');
 
 // creating a bot from the telegraf package
-const bot = new Telegraf(process.env.BOT_API_KEY)
-
-// middleware
-// TODO Add welcome message
+const bot = new Telegraf(process.env.BOT_API_KEY);
 
 // Writing a handler for the /start command
 // TODO get Telegram username and check if the user already exists
@@ -27,17 +24,22 @@ bot.start((ctx) => {
     Add them by typing add cookie
     When you're feeling down, have a cookie.
     Request a cookie by typign cookie please
-    Resume your badassery!`)
+    Resume your badassery!`);
 
-    console.log(ctx)
 })
 
 bot.help((ctx) => {
-    ctx.reply(`I'm happy to help!`)
+    ctx.reply(`I'm happy to help!`);
 })
 
 bot.settings((ctx) =>{
-    ctx.reply(`It's YOUR cookie jar. Set it up how you need it!`)
+    ctx.reply(`It's YOUR cookie jar. Set it up how you need it!`);
+})
+
+// Create a custom command
+// The command responds to both /test and /Test
+bot.command(["test", "Test"], (ctx) => {
+    ctx.reply("This is a test command. Yay!");
 })
 
 bot.launch()
